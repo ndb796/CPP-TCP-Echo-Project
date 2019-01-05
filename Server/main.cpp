b86b3e4177fb1925fd66741acbdb1f70b8cf9c21
ndb796 <ndb796@naver.com>
2019-01-05 17:25:15 +0900

    Update Server's main.cpp

--- Server/main.cpp
+++ Server/main.cpp
@@ -13,6 +13,7 @@ void ShowErrorMessage(string message)
 
 int main()
 {
+	cout << "[C++ TCP Echo Project Ver 0.2]\n";
 	WSADATA wsaData;
 	SOCKET serverSocket, clientSocket;
 	SOCKADDR_IN serverAddress, clientAddress;
